d445c6b5a00ac1f4942f76f6ee4c514a392c0c57
Vladimir Mironovich <ninerlp@gmail.com>
2012-12-13 22:33:33 +0400

    just another commit
    tester is impossibru

--- unsed/readlines-main.c
+++ unsed/readlines-main.c
@@ -1,14 +1,15 @@
 #include "readlines.h"
  //trying to use named pipes for testing
 int main(int argc, char *argv[]){
-        int fd_out = open("testpipeout",O_WRONLY);
+        //int fd_out = open("testpipeout",O_WRONLY);
+        int fd_out = 0;
         if (argc < 2){
                 printf("No max_size given");
                 return 0;
         }
         int max_size = atoi(argv[1]);
-        //struct RL* rl = rl_open(1, max_size);
-        struct RL* rl = rl_open(open("testpipeto",O_RDONLY),max_size);
+        struct RL* rl = rl_open(1, max_size);
+        //struct RL* rl = rl_open(open("testpipeto",O_RDONLY),max_size);
         int res = rl_readline(rl, rl->buf, rl->buf_size);
         while (res != 0){
                 if (res > 0) {
