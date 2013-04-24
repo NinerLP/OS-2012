d2504a37f7940023c625080a9ce6e454f7d29775
NinerLP <ninerlp@gmail.com>
2013-04-25 01:15:30 +0400

    copy paster stuff, getting a hang of poll()

---
+++ copy-paster/cp-poll.c
@@ -0,0 +1,58 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/sendfile.h>
+#include <poll.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+int main(int argc, const char* argv[] ) {
+	/* so we have file descriptor list in argv
+	 * 2n of fd, and we use sendfile(argv[2k-1],argv[2k],NULL, sizeoffd?
+	 * poll usage, do i just make arry of structs 
+	 * fd - events - revents for each fd, where i set events POLLIN and POLLOUT for 2k and 2k-1 fds?
+	 * FROM 2k-1 TO 2k
+	*/
+	
+	//rough shit that may be working or may be not working at all
+	int num = (argc-1)/2;
+	struct stat* buf = (struct stat*)malloc(sizeof(struct stat));
+	printf("%d\n", num);
+	struct pollfd* fds =(struct pollfd*)malloc(sizeof(struct pollfd)*num*2);
+	int i;
+	for (i = 1; i <= num; i++) {
+		fds[2*i-1].fd = atoi(argv[2*i-1]);
+		fds[2*i-1].events = POLLOUT;
+		fds[2*i].fd = atoi(argv[2*i]);
+		fds[2*i].events = POLLIN;
+		printf("From fd %d to %d\n", fds[2*i-1].fd, fds[2*i].fd);
+	}
+	int parsed = 0;
+	int res;
+	while (parsed < num) {
+		res = poll(fds,num*2,1000);
+		if (res < 0) {
+			//error
+		} else {
+			printf("Dun gufed %d\n", res);
+			for (i = 1; i <= num; i++) {
+				if (fds[2*i-1].revents) {
+					printf("fD %d rdy\n", fds[2*i-1].fd);
+				}
+				if (fds[2*i].revents) {
+					printf("fD %d rdy\n", fds[2*i].fd);
+				}
+				if ((fds[2*i-1].revents & POLLOUT) && (fds[2*i].revents & POLLIN)) {
+					fstat(fds[2*i-1].fd,buf);
+					sendfile(fds[2*i-1].fd,fds[2*i].fd, NULL, buf->st_size);
+					fds[2*i-1].events = 0;
+					fds[2*i].events = 0;
+					parsed++;
+				}
+			}
+		}
+	}
+	return 0;
+}
